02216078e49bf008a734db2698bcbd00b3aad60f
Grimmvh <conradtvh@gmail.com>
2021-02-24 10:28:31 +0200

    init

--- prg381.java
+++ prg381.java
@@ -5,5 +5,6 @@ public class prg381 {
 
     public static void main(String[] args) {
         System.out.println("First Java file on git");
+        System.out.println("First Java file on git");
     }
 }
