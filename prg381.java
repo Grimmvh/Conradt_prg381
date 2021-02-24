9feedf698d30ad116e8f38ec4e58e3f6d370c643
Grimmvh <conradtvh@gmail.com>
2021-02-24 10:35:33 +0200

    removed comments

--- prg381.java
+++ prg381.java
@@ -1,11 +1,9 @@
-/**
- * prg381
- */
 public class prg381 {
 
     public static void main(String[] args) {
         System.out.println("First Java file on git");
         System.out.println("First Java file on git");
+        // comment
         // comment
     }
 }
